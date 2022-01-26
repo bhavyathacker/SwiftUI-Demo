2e0e5f2983867d524a6378b94c7d98e6b87ba459
Bhavya Thacker <bhavyathacker@gmail.com>
2022-01-26 20:29:52 +0530

    text change

--- SwiftUI Demo/ContentView.swift
+++ SwiftUI Demo/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
                 Spacer().frame(width: 5).background(Color.red)
                 HStack(spacing: 12){
                     Spacer().frame(height: 5).background(Color.red)
-                    Text("Bottom Left").padding(.all,10.0).background(Color.cyan)
+                    Text("Bottom Right").padding(.all,10.0).background(Color.cyan)
 
                 }
             }.padding().background(Color.yellow)
