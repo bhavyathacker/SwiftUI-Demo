f9c5cc79093068e17a2b937a2592dba11a51f3c0
Bhavya Thacker <bhavyathacker@gmail.com>
2022-01-26 20:28:11 +0530

    Initial commit

---
+++ SwiftUI Demo/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Admin on 26/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.green.edgesIgnoringSafeArea(.all)
+            VStack{
+                HStack(spacing: 12.0){
+                    Text("Top Left").padding(.all,10.0).background(Color.cyan)
+                    Spacer().frame(height: 5).background(Color.red)
+
+                }
+                Spacer().frame(width: 5).background(Color.red)
+                HStack(spacing: 12){
+                    Spacer().frame(height: 5).background(Color.red)
+                    Text("Bottom Left").padding(.all,10.0).background(Color.cyan)
+
+                }
+            }.padding().background(Color.yellow)
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI Demo
+//
+//  Created by Admin on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
